b1bdb081cff7e23037a6058af942a692536bb80d
apeden <apeden23@gmail.com>
2019-02-26 19:04:43 +0000

    The two main py files have be modified to allow the calculation of mean results for two adjacent rows of data, and also to return positive or negative statements for the rtquic row data files

--- RTQuicSheet.py
+++ RTQuicSheet.py
@@ -1,14 +1,13 @@
-####COLLECTION OF FUNCTIONS FOR ANALYSING RTQUIC DATA FROM EXCEL####
-####ALEX PEDEN, DECEMBER 2018#####
+"""COLLECTION OF FUNCTIONS FOR ANALYSING RTQUIC DATA FROM EXCEL
+ALEX PEDEN, DECEMBER 2018"""
 
 from openpyxl import load_workbook
-
+import statistics
 
 class RTQuicSheet(object):
     
-
-    SECONDS_PER_CYCLE = 949
     def __init__(self, excel_workbook_name, sheet_name):
+        self.SECONDS_PER_CYCLE = 949
         #clear previous workbook
         self.wb = None
         self.excel_workbook_name = excel_workbook_name
@@ -20,55 +19,84 @@ class RTQuicSheet(object):
             self.wb = None
             print("Could not open file"+ self.excel_workbook_name)
         ##get sheet as file object
-    
-        self.sheet = self.wb[self.sheet_name]
+        try:
+            self.sheet = self.wb[self.sheet_name]
+        except:
+            print("Could not find sheet in source data file")
         self.row_label = ""
         self.row_list = []
-        self.row_max = None
+        self.column_list = []
+        self.row_max = None ##eventually will be flourescence units
         self.time_to_max = None ##eventually will be in seconds
-        self.threshold = None 
+        self.threshold = None ##eventually will be flourescence units
         self.lag = None ##eventually will be in seconds
+        self.baseMean = 0.0
+        self.baseSTDEV = 0.0
+        self.baseline = 0.0
+        self.time_to_baseline = None ##eventually will be in seconds
+        self.time_baseline_to_max = None ##eventually will be in seconds
+        self.gradient = 0.0 ## fluoresecent units/sec
 
+    def getSECONDS_PER_CYCLE(self):
+        return self.SECONDS_PER_CYCLE
 
-    """gets a work book object"""
-    def get_wb(self):
-        
-        return wb
-    
     def set_row_label(self, row):
         try:
             val = self.sheet.cell(row, column=1).value
-        except AttributeError:   
+        except:
             print("A problem occurred setting the row label.")
         self.row_label = val
 
     def get_row_label(self):
         return self.row_label
 
+    def get_cell_val(self, row, column):
+        try:
+            val = self.sheet.cell(row, column).value
+        except:
+            print("A problem occurred reading data from a row")
+        return val
 
     """
-    reads a selected row from a sheet file object and returns a list of what's in it.
-    row_start refers the column number i.e. A = 1, B = 2 etc where you want to start
+    reads a selected row (up to 1000 cells) from a sheet file object and returns a list of what's in it.
+    column_start refers the column number i.e. A = 1, B = 2 etc where you want to start
     reading the row (left to right).
+    breaks when an empty cell is encountered
     """
-    def set_row_list(self, row_start, row):
+    def set_row_list(self, column_start, row):
         print("row number sent to set_row_list "+str(row))
         self.row_list = []
-        for i in range(row_start, 1000):        
-            try:
-                val = self.sheet.cell(row, column=i).value
-            except AttributeError:   
-                print("A problem occurred reading data from a row")
-                
+        for column in range(column_start, 1000):
+            val = self.get_cell_val(row, column)
             if val == None:
                 break
             self.row_list = self.row_list + [val]
-            
         print(self.row_list[0:20])
 
     def get_row_list(self):
         return self.row_list
-    
+
+    """
+    reads a selected column from a sheet file object and returns a list of what's in it.
+    column_start refers the row number where you want to start reading downwards from
+    breaks when an empty cell is encountered
+    """
+    def set_column_list(self, column, row_start):
+        print("column number sent to set_column_list "+str(column))
+        self.column_list = []
+        val = 0
+        row = row_start
+        while True:
+            val = self.get_cell_val(row, column)
+            if not type(val) == int:
+                break
+            self.column_list = self.column_list + [val]
+            row += 1
+        print(self.column_list[0:20])
+
+    def get_column_list(self):
+        return self.column_list
+
     """
     reads from a row as a list of ints and/or floats and obtains maximum value
     and time (in hours) when max value occurred
@@ -88,14 +116,15 @@ class RTQuicSheet(object):
                 print("item in row list is not a number")
                 self.row_max = None
         print("Now it's " +str(self.row_max))
-                
+
     def get_row_max(self):
         return self.row_max
 
+    def calculate_time_sec(self, start, end):
+        return (end-start)*self.SECONDS_PER_CYCLE
+
     def set_time_to_max(self):
-        ## in seconds
-        self.set_row_max()
-        self.time_to_max = self.row_list.index(self.row_max)*(RTQuicSheet.SECONDS_PER_CYCLE) 
+        self.time_to_max = self.calculate_time_sec(0, self.row_list.index(self.row_max))
 
     def get_time_to_max(self):
         return self.time_to_max
@@ -109,35 +138,71 @@ class RTQuicSheet(object):
         assert(type(self.row_list[2]) == int or type(self.row_list[2]) == float)
         self.threshold = self.row_list[2]*3
 
+    def getThreshold(self):
+        return self.threshold
+
     def setLag(self):
-        self.setThreshold()
         self.lag = 0
         for i in range(len(self.row_list)-2):
             if (self.row_list[i] > self.threshold and
                 self.row_list[i+1] > self.threshold and
                 self.row_list[i+2] > self.threshold):
-                self.lag = i*(RTQuicSheet.SECONDS_PER_CYCLE)##in seconds
-                return           
+                self.lag = self.calculate_time_sec(0,i)##in seconds
+                return
         print ("no lagtime found for row ")
-        
+
     def getLag(self):
-            return  self.lag ##in seconds
+        return self.lag
+
+    def setBaseMean(self):
+        self.baseMean = statistics.mean(self.column_list)
+
+    def getBaseMean(self):
+        return self.baseMean
+
+    def setBaseSTDEV(self):
+        self.baseSTDEV = statistics.stdev(self.column_list)
+
+    def getBaseSTDEV(self):
+        return self.baseSTDEV
+
+    def setBaseline(self):
+        self.baseline = self.baseMean + 3* self.baseSTDEV
+
+    def getBaseline(self):
+        return self.baseline
+
+    def set_time_to_baseline(self):
+        self.time_to_baseline = 0
+        for i in range(len(self.row_list)):
+            if self.row_list[i] > int(self.baseline):
+                 self.time_to_baseline = self.calculate_time_sec(0, i)
+                 break
+
+    def get_time_to_baseline(self):
+        return self.time_to_baseline
+
+    def set_time_baseline_to_max(self):
+        self.time_baseline_to_max = self.time_to_max - self.time_to_baseline
+
+    def get_time_baseline_to_max(self):
+        return self.time_baseline_to_max
+
+    def set_gradient(self):
+        if self.time_baseline_to_max > 0:
+            self.gradient = (self.row_max - self.baseline)/self.time_baseline_to_max
+
+    def get_gradient(self):
+        return self.gradient
+
+    def is_positive(self):
+        return (self.lag > 0 and self.lag < 360000)
 
     "takes a time in seconds and converts to h:m format"
     def hours(self, time_sec):
-        hours = str(time_sec//3600) 
+        hours = str(time_sec//3600)
         minutes = str(time_sec%3600//60)
         return hours +" hours: "+ minutes + " minutes"
 
-
     def __str__(self):
         return "Workbook: "+ self.excel_workbook_name + " Sheet: " + self.sheet_name
-        
-
-
-
-
-
-
-
-
--- rtquicSheetAnalyser.py
+++ rtquicSheetAnalyser.py
@@ -2,11 +2,20 @@ from openpyxl import Workbook
 from os import listdir
 from RTQuicSheet import *
 
+class Error(Exception):
+    pass
 
+class FileNotSavedError(Error):
+    pass
+
+PATH_TO_FILES = "C:\\Users\\apeden\\OneDrive - University of Edinburgh\\excelMaxRow\\RTQuIC_for_analysis"
+list_of_files = (listdir(PATH_TO_FILES))
+
+"""Analyses data in an excel file row by row
+Puts results in a destination excel file"""
 class SheetAnalyser(object):
-    PATH_TO_FILES = "C:\\Users\\apeden\\OneDrive - University of Edinburgh\\excelMaxRow\\RTQuIC_for_analysis"
-    list_of_files = (listdir(PATH_TO_FILES))      
     def __init__(self, raw_data, raw_data_sheet):
+        self.analysis_num = 0
         self.row_num = 2 #row in destination excel file
         self.raw_data = raw_data
         self.raw_data_sheet = raw_data_sheet
@@ -25,41 +34,63 @@ class SheetAnalyser(object):
         except:
             print("Could not generate active worksheet within distination workbook for " + self.raw_data)      
             raise AttributeError
+        
+    def set_analysis_num(self, num):
+        self.analysis_num = num
 
-    def set_result(self, row_start, row):
+    def set_sheet_baseline(self):
+        self.rtquic1.set_column_list(column_origin,row_origin)
+        if len(self.rtquic1.get_column_list()) > 0:
+            self.rtquic1.setBaseMean()
+            self.rtquic1.setBaseSTDEV()
+            self.rtquic1.setBaseline()
+        
+
+    def set_result(self, column_start, row):##read row left to right from start point = column_start
         self.rtquic1.set_row_label(row)
-        self.rtquic1.set_row_list(row_start, row)
+        self.rtquic1.set_row_list(column_start, row)
         #check a row_list was generated
         if len(self.rtquic1.get_row_list()) == 0:
             raise ValueError   
         self.rtquic1.set_row_max()
         self.rtquic1.set_time_to_max()
+        self.rtquic1.setThreshold()
         self.rtquic1.setLag()
-        ##below time results (time to max and lag) in seconds    
-
+        if len(self.rtquic1.get_column_list()) > 0:
+            self.rtquic1.set_time_to_baseline()
+            self.rtquic1.set_time_baseline_to_max()
+            self.rtquic1.set_gradient()      
+        
 
     def get_result(self):
         return (self.rtquic1.get_row_label(),
                 self.rtquic1.get_row_max(),
                 self.rtquic1.get_time_to_max(),
-                self.rtquic1.getLag())        
+                self.rtquic1.getLag(),
+                self.rtquic1.get_gradient(),
+                self.rtquic1.is_positive())
         
-
-    def set_result_list(self, row_start, row):
+    def set_result_list(self, column_start, row):
         print("row number sent to set result list "+str(row))
         self.result_list = []
         self.row_num += 1
-        self.set_result(row_start, row)
+        self.set_result(column_start, row)
         Label = self.rtquic1.get_row_label()
         maxVal = self.rtquic1.get_row_max()
         maxHours = self.rtquic1.hours(self.rtquic1.get_time_to_max())
         lagHours = self.rtquic1.hours(self.rtquic1.getLag())
-        #list of mean values with times in hours
+        gradient = self.rtquic1.get_gradient()
+        result = ""
+        if self.rtquic1.is_positive():
+            result = "Positive --yeah!"
+        else:
+            result = "Negative"
         self.result_list = [Label,
                             maxVal,
                             maxHours,
-                            lagHours]
-
+                            lagHours,
+                            gradient,
+                            result]
 
     def row_filler(self):
         try:
@@ -71,7 +102,12 @@ class SheetAnalyser(object):
             print("Could not fill a row")
 
     def data_label_row_filler(self):
-        data_labels = ["Label", "Max Val", "Time to Max", "Lag time"] 
+        data_labels = ["Label",
+                       "Max Val",
+                       "Time to Max",
+                       "Lag time",
+                       "Gradient",
+                       "Result"] 
         self.ws.cell(row =1, column =1, value = self.raw_data)
         try:
             for i in range(len(data_labels)):
@@ -82,18 +118,23 @@ class SheetAnalyser(object):
             print("Could not fill title row")
 
     def save_wb(self):
-        self.wb.save(self.__str__())
+        try:
+            self.wb.save(self.__str__()+".xlsx")
+        except:
+            print("An error occurred when I tried to save the file")
+            raise FileNotSavedError
 
     def __str__(self):
         source_file = self.raw_data.replace("-", "_")
         source_file = source_file.replace(" ", "_")
-        return ("Analysis_of_"+ "string")
+        return ("Analysis_of_File"+ str(self.analysis_num))
         
-    def analyse(self, first_row, last_row, first column, last_column): #first and last row in source file
+    def analyse(self, first_row, last_row, column_start): #first and last row in source file
         self.data_label_row_filler()
-        for i in range(first_row, last_row + 1,):
+        self.set_sheet_baseline()
+        for row in range(first_row, last_row + 1,):
             try:
-                self.set_result_list(first_column, last_column + 1)
+                self.set_result_list(column_start, row)
             except ValueError:
                 self.save_wb()
                 print("had to stop, but file still saved")
@@ -101,27 +142,23 @@ class SheetAnalyser(object):
             self.row_filler()
         self.save_wb()
 
-    def save_file(sheet_analyser):
-        try:
-            sheet_analyser.save_wb() 
-        except PermissionError:
-            print("Could not save to default result file")
 
-
-
+"""Analyses data in an excel file duplicate row by duplicate row
+Puts results in a destination excel file"""
 class SheetAnalyserMeans(SheetAnalyser):
                 
     def set_result_list(self, row_start, row):
         self.result_list = []
-        self.row_num += 2
+        self.row_num += 1
         self.set_result(row_start, row)
+        Label = self.rtquic1.get_row_label()
         upper_row  = self.get_result()
         self.set_result(row_start, row+1)
         lower_row = self.get_result()
         mean_row = ()
         for i in range (1,len(upper_row)):
             mean_row = mean_row +(((upper_row[i]+ lower_row[i])/2),)
-        Label = self.rtquic1.get_row_label()
+        
         maxVal = mean_row[0]
         maxHours = self.rtquic1.hours(mean_row[1])
         lagHours = self.rtquic1.hours(mean_row[2])
@@ -130,9 +167,63 @@ class SheetAnalyserMeans(SheetAnalyser):
                             maxVal,
                             maxHours,
                             lagHours]
-        
 
+    def analyse(self, first_row, last_row, column_start): #first and last row in source file
+        self.data_label_row_filler()
+        self.set_sheet_baseline()
+        for row in range(first_row, last_row + 1, 2):
+            try:
+                self.set_result_list(column_start, row)
+            except ValueError:
+                self.save_wb()
+                print("had to stop, but file still saved")
+                return
+            self.row_filler()
+        self.save_wb()
+
+
+
+      
+##Scope of sheet
+row_origin = 13
+row_end = 108
+column_origin = 4
+
+##METHOD FOR ANALYSING AN INDIVIDUAL EXCEL FILE
+t = SheetAnalyser(PATH_TO_FILES+"\\"+"RTQUIC_READ_19_004.xlsx", "All_Cycles")                
+t.analyse(row_origin, row_end, column_origin)
+
+##METHOD FOR ANALYSING A FOLDER OF EXCEL FILES
+##t= None
+##for i in range(len(list_of_files)): 
+##    if t != None:
+##        del t 
+##    t = SheetAnalyser(PATH_TO_FILES+"\\"+list_of_files[i], "Results")
+##    t.set_analysis_num(i) #keeps track of excel sheets analysed, for generating separate destination files
+##    t.analyse(13, 108, 14)
     
+##ANALYSIS OF ONE COLUMN FROM ONE SHEET
 
-
-analyse(PATH_TO_FILES+"\\"+list_of_files[0], "Results") ## NEED TO CHANGE THIS
+##t = RTQuicSheet(PATH_TO_FILES+"\\"+list_of_files[0], "Results")
+##t.set_column_list(14,13)
+##t.setBaseMean()
+##t.setBaseSTDEV()
+##t.setBaseline()
+##print("BaseMean is " + str(t.getBaseMean()))
+##print("BaseSTDEV is " + str(t.getBaseSTDEV()))
+##print("Baseline is " + str(t.getBaseline()))
+##t.set_row_list(14,16)
+##t.set_row_max()
+##print("Row max is " + str(t.get_row_max()))
+##t.set_time_to_max()
+##print("Time to max is " + str(t.get_time_to_max()))
+##t.setThreshold()
+##print("Threshold is " + str(t.getThreshold()))
+##t.setLag()
+##print("Lag is " + str(t.getLag()))
+##t.set_time_to_baseline()
+##print("Time to baseline is " + str(t.get_time_to_baseline()))
+##t.set_time_baseline_to_max()
+##print("Time of baseline to max is " + str(t.get_time_baseline_to_max()))
+##t.set_gradient()
+##print("Gradient is " + str(t.get_gradient()))
