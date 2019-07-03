8379003358d2cdde9a0b0e54d47aa7f9a220e1e7
apeden <apeden23@gmail.com>
2019-07-03 14:56:07 +0100

    able to plot ranges or individual traces

--- rtquicPlotter.py
+++ rtquicPlotter.py
@@ -6,14 +6,34 @@ from RTQuicSheet import *
 line graphs. x axis: 0 to 100 hours, y axis 0 to
 260000 F Units"""
 
-def plotRange(file, start_row, end_row):
-    x_values, y_values  = [],[]
+
+def getResults(file):
     testSheet = RTQuicSheet("RTQuIC Review/"+file, "Results")
     testData = RTQuICData(testSheet.getSheet(), 13, 9,
                       label_col = 1, numRows = 96)
     plateData = testData.getData()
     data_labels = testData.getLabels()
     cycle_time = testData.getSecPerCyc()/3600 # in hour
+    return plateData, data_labels, cycle_time
+
+def display(title, xlabel, ylabel, base = None):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if not base == None:
+        plt.axhline(base)
+    plt.legend()
+    plt.show()
+
+
+title= 'RT-QuIC Fluorescence trace'
+xlabel='Hours'
+ylabel='Flourescence Units'
+
+def plotRange(file, start_row, end_row, title, xlabel, ylabel):
+    x_values, y_values  = [],[]
+    plateData, data_labels, cycle_time\
+               = getResults(file)
     for i in range(len(plateData[0])):
         time_hours = (i+1)*cycle_time
         x_values.append(time_hours)    
@@ -21,15 +41,27 @@ def plotRange(file, start_row, end_row):
         label, row_data = data_labels[i], plateData[i]   
         y_values = row_data
         plt.plot(x_values, y_values, label = str(label))
-    plt.title('RT-QuIC Fluorescence trace')
-    plt.xlabel('Hours')
-    plt.ylabel('Flourescence Units')
-    plt.axhline(100000)
-    plt.legend()
-    plt.show()
+    display(title, xlabel, ylabel)
+
+
+def plotOne(file, row, title, xlabel, ylabel, base = False):
+    x_values, y_values  = [],[]
+    plateData, data_labels, cycle_time\
+               = getResults(file)
+    for i in range(len(plateData[row])):
+        time_hours = (i+1)*cycle_time
+        x_values.append(time_hours)    
+    label, row_data = data_labels[row], plateData[row]   
+    y_values = row_data
+    plt.plot(x_values, y_values, label = str(label))
+    if base:
+        display(title, xlabel, ylabel, base = row_data[2])
+    else:
+        display(title, xlabel, ylabel)
+
 
 
 file = "Experimental plan RTQUIC19 004 AHP 65+study cases  37 38 40 39 41 42 01 02.xlsx"
-plotRange(file, start_row = 0, end_row = 96)
-
+plotRange(file, 0, 8, title, xlabel, ylabel)
+#plotOne(file, 1, title, xlabel, ylabel, True)
 
